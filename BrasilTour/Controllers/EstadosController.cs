e2e070c1a2bf867c950fc42b3434671259744e02
user <vitorfds18@gmail.com>
2022-10-16 16:04:22 -0300

    atualização html

--- BrasilTour/Controllers/EstadosController.cs
+++ BrasilTour/Controllers/EstadosController.cs
@@ -1,4 +1,44 @@
-﻿namespace BrasilTour.Controllers {
-    public class EstadosController {
+﻿using BrasilTour.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using System.Diagnostics;
+
+namespace BrasilTour.Controllers {
+    public class EstadosController : Controller {
+       
+
+            public IActionResult Index() {
+
+
+                return View();
+            }
+
+
+        public IActionResult RioDeJaneiro() {
+
+
+            return View();
+        }
+
+
+        public IActionResult Rondonia() {
+
+
+            return View();
+        }
+
+
+
+        public IActionResult Privacy() {
+                return View();
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error() {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
     }
-}
+
+
---
+++ BrasilTour/Models/EstadosModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BrasilTour.Models {
+    public class EstadosModel {
+        public string NomeEstado { get; set; }
+
+        public string InfoEstado { get; set; }
+    }
+}
